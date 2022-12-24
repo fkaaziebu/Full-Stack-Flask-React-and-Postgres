185d3e46e77cb8e5c35f81982ef8c06dee6b6cb0
fkaaziebu <frederickaziebu1998@gmail.com>
2022-12-24 18:46:51 +0000

    SQLite database

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+from datetime import datetime
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Event: {self.description}"
+
+    def __init__(self, description):
+        self.description = description
+    
 
 @app.route('/')
 def hello():
+    db.create_all()
     return 'Hey'
 
 if __name__ == '__main__':
