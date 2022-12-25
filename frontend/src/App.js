3995853bb8eea9db619b4bdc34d2e18f7e603a07
fkaaziebu <frederickaziebu1998@gmail.com>
2022-12-25 01:33:01 +0000

    Creating the UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from "axios";
+import { format, set } from "date-fns";
+
 import './App.css';
 
+const baseUrl = "http://localhost:5000";
+
 function App() {
+  const [description, setDescription] = useState("");
+
+  const handleChange = (e) => {
+    setDescription(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(description)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="description">Description</label>
+        <input
+          onChange={handleChange}
+          type="text"
+          name="description"
+          id="description"
+          value={description}
+        />
+        <button type="submit">Submit</button>
+      </form>    
     </div>
   );
 }
