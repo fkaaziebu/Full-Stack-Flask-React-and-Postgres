5603219ba5ec9ab2bb2bc21468337d73a99ddd12
fkaaziebu <frederickaziebu1998@gmail.com>
2022-12-25 02:54:12 +0000

    Updating the database
    Update to the database successful

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,18 +9,39 @@ const baseUrl = "http://localhost:5000";
 function App() {
   const [description, setDescription] = useState("");
   const [eventsList, setEventsList] = useState([]);
+  const [eventId, setEventId] = useState(null);
+  const [editDescription, setEditDescription] = useState("");
 
 
-  const handleChange = (e) => {
-    setDescription(e.target.value)
+
+  const handleChange = (e, field) => {
+    if (field === "edit") {
+      setEditDescription(e.target.value)
+    } else {
+      setDescription(e.target.value)
+    }
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const data = await axios.post(`${baseUrl}/events`, {description})
-      setEventsList([...eventsList, data.data]);
+      if (editDescription) {
+        const data = await axios.put(`${baseUrl}/events/${eventId}`, {description: editDescription})
+        const updatedEvent = data.data.event;
+        const updatedList = eventsList.map(event => {
+          if (event.id === eventId) {
+            return event = updatedEvent;
+          }
+          return event;
+        })
+        setEventsList(updatedList);
+      } else {
+        const data = await axios.post(`${baseUrl}/events`, {description})
+        setEventsList([...eventsList, data.data])
+      }
       setDescription("");
+      setEditDescription("");
+      setEventId(null)
     } catch {
       // console.error(e.message);
     }
@@ -34,6 +55,12 @@ function App() {
     } catch {
       // console.error(err.message)
     }
+  }
+
+  // Toggle edit function
+  const toggleEdit = (event) => {
+    setEventId(event.id);
+    setEditDescription(event.description)
   }
 
   // Fetching data from the API
@@ -64,12 +91,30 @@ function App() {
       <section>
         <ul>
           { eventsList.map(event => {
-            return (
-              <li key={event.id}>
-                {event.description}
-                <button onClick={() => handleDelete(event.id)}>X</button>
+            if (eventId === event.id) {
+              return (
+                <li key={eventId}>
+                  <form onSubmit={handleSubmit}>
+                    <input
+                      onChange={(e) => handleChange(e, 'edit')}
+                      type="text"
+                      name="editDescription"
+                      id="editDescription"
+                      value={editDescription}
+                    />
+                    <button type="submit">Submit</button>
+                  </form>
                 </li>
-            )
+              )
+            } else {
+              return (
+                <li key={event.id}>
+                  {event.description}
+                  <button onClick={() => toggleEdit(event)}>Edit</button>
+                  <button onClick={handleDelete}>X</button>
+                </li>
+              )
+            }
           })}
         </ul>
       </section>
